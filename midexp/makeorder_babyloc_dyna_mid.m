0baabcbc63e272d9dfce49779743a4d91412f888
christina tyagi <102990003+cgtyagi@users.noreply.github.com>
2023-11-01 12:29:14 -0700

    Update makeorder_babyloc_dyna_mid.m

--- midexp/makeorder_babyloc_dyna_mid.m
+++ midexp/makeorder_babyloc_dyna_mid.m
@@ -132,18 +132,26 @@ for r = 1:nruns
 };
 
     %Shuffle the order of actors for the cars category
-    caract{r} = shuffle({'blackexcavator', 'blueexcavator', 'dumptruck', 'car', 'yellowexcavator', 'rccar', 'racecar'});
+    caract{r} = shuffle({'blackexcavator', 'blueexcavator', 'dumptruck', 'car1', 'car2', 'car3', 'car4', 'car5', 'car6', 'car7', 'car8', 'car9', 'yellowexcavator', 'rccar', 'racecar'});
     %Define the video ranges for different types of cars 
     carVideoRanges = {
     'blackexcavator', 9;
     'blueexcavator', 12;
     'dumptruck', 9;
-    'car', 10; 
+    'car1', 2; 
+    'car2', 1;
+    'car3', 1; 
+    'car4', 1; 
+    'car5', 1; 
+    'car6', 1;
+    'car7', 1; 
+    'car8', 1; 
+    'car9', 1; 
     'yellowexcavator', 1;
     'rccar', 2; 
     'racecar', 1;
 };
-
+    
     %Shuffle the order of actors for the scenes category; all have one
     %video so no need to define video ranges
     sceneact{r} = shuffle({'forestwaterfalllow', 'autumnforeststream', 'beachwithdriftwood', 'bigwaterfallinforest', 'birchforeststream', 'blueforeststream', 'bluemistystream', 'bluerapidsintostream', 'bluewaves', 'calmbeach', 'dam', 'evergreenforeststream', 'fastforestrapids', 'foggyaerialbeach', 'forestblueriver', 'forestwaterfall', 'junglesmallwaterfall', 'mossyrockstream', 'mountainbeachwaves', 'mountainrain', 'mountainstreamblue', 'muddystream', 'powerfulwaterfall', 'powerlinewater', 'rivernearbridge', 'riverwithflowers', 'rockybeach', 'rockycliffbeach', 'rockystreamhills', 'sandybeachwaves', 'shadybluewaterfall', 'slowjunglestream', 'smallmossyrockstream', 'smallshallowmossyrockstream', 'snowingtalltrees', 'snowingwithshed', 'snowyfield', 'snowyfield', 'snowylawn', 'streambed', 'sunnyclouds', 'sunnyriver', 'sunnywaves', 'sunsetclouds', 'topofmountainsclouds', 'tropicalbeach', 'twilightbeach', 'waterfallwithbluepond', 'widejunglewaterfall', 'widejunglewaterfall'})
