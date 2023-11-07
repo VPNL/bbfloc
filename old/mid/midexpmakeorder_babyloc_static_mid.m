9b749c8d2d54fdb166c7667387a5d64a58702c54
christina tyagi <102990003+cgtyagi@users.noreply.github.com>
2023-11-07 11:59:34 -0800

    Update midexpmakeorder_babyloc_static_mid.m

--- old/mid/midexpmakeorder_babyloc_static_mid.m
+++ old/mid/midexpmakeorder_babyloc_static_mid.m
@@ -1,4 +1,4 @@
-%% this version doesn't have a random pattern img for the last trial 
+%% this version has incorrect indexing; doesn't have a random pattern img for the last trial 
 
 function makeorder_babyloc_static_mid(participant)
 %% Generates 4 runs/CSV scripts for functional
