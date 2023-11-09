5252fece2f45fa5f508af2180f059680df608da8
christina tyagi <102990003+cgtyagi@users.noreply.github.com>
2023-11-09 15:14:22 -0800

    Update makeorder_babyloc_dyna_long.m

--- longexp/makeorder_babyloc_dyna_long.m
+++ longexp/makeorder_babyloc_dyna_long.m
@@ -1,8 +1,8 @@
-function makeorder_babyloc_dyna_long(participant)
+function makeorder_babyloc_dyna_long(participant, user)
 %% Generates 4 runs/CSV scripts for dynamic condition for the infant scans containing 2 stimuli per block with presentation rates of
 %% long.
 %
-% INPUT: Should be the baby's number 
+% INPUT: Should be the baby's number and the user of the laptop
 % OUTPUTS: Separate script file for each run of PsychoPy experiment.
 %
 % STIMULI: 5 stimulus conditions (aka categories) 
@@ -40,7 +40,7 @@ ntrials = nblocks*stimsperblock; % number of trials in a run
 blockdur = stimsperblock*stimdur; % block duration (sec)
 rundur = nblocks*blockdur; % run duration (sec)
 
-participant_folder = fullfile('/Users', 'vpnl', 'Desktop', 'alternating_bb', 'alternating_PsychoPy', 'data', participant, 'long');
+participant_folder = fullfile('/Users', user, 'Desktop', 'alternating_bb', 'alternating_PsychoPy', 'data', participant, 'long');
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -73,8 +73,8 @@ for r = 1:nruns
     condmat(:, r) = reshape(repmat(condvec', stimsperblock, 1), ntrials, 1);
 end
 
-stim_dir = '/Users/vpnl/Desktop/alternating_bb/alternating_stimuli/dynamic_stimuli';
-blank_video_path = '/Users/vpnl/Desktop/alternating_bb/alternating_stimuli/dynamic_stimuli/blank/blank.mp4'
+stim_dir = fullfile('/Users', user, 'Desktop', 'alternating_bb', 'alternating_stimuli', 'dynamic_stimuli');
+blank_video_path = fullfile(stim_dir, 'blank', 'blank.mp4');
 
 % Create matrix for Image
 vidmat = cell(ntrials,nruns);
@@ -251,7 +251,7 @@ end
 
 
 % Path to the directory containing video files
-video_directory = fullfile('/Users', 'vpnl', 'Desktop', 'alternating_bb',  'alternating_stimuli', 'dynamic_stimuli')
+video_directory = fullfile('/Users', user, 'Desktop', 'alternating_bb',  'alternating_stimuli', 'dynamic_stimuli')
 
 % Map the original category index to a new index
 category_mapping = [6, 7, 8, 9, 0];
