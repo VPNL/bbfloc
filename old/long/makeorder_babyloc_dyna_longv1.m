56534bfdd447b50027461ab67bf201e4da2cb209
christina tyagi <102990003+cgtyagi@users.noreply.github.com>
2023-11-09 10:38:04 -0800

    Update makeorder_babyloc_dyna_longv1.m

--- old/long/makeorder_babyloc_dyna_longv1.m
+++ old/long/makeorder_babyloc_dyna_longv1.m
@@ -1,4 +1,5 @@
-%incorrect indexing
+%incorrect indexing of category
+% categories can have consecutive repeats
 
 function makeorder_babyloc_dyna_long(participant)
 %% Generates 8 runs/CSV scripts for dynamic condition for the infant scans containing 2 stimuli per block
