9fa5390d2308ca92acb0bd9974280414f4eae9a9
christina tyagi <102990003+cgtyagi@users.noreply.github.com>
2023-11-09 10:33:28 -0800

    Update and rename midexpmakeorder_babyloc_dyna_mid.m to midexpmakeorder_babyloc_dyna_midv1.m

--- old/mid/midexpmakeorder_babyloc_dyna_midv1.m
+++ old/mid/midexpmakeorder_babyloc_dyna_midv1.m
@@ -1,4 +1,4 @@
-% this version has incorrect indexing 
+% this version has incorrect indexing of categories
 
 function makeorder_babyloc_dyna_mid(participant)
 %% Generates 8 runs/CSV scripts for dynamic condition for the infant scans containing 2 stimuli per block 
