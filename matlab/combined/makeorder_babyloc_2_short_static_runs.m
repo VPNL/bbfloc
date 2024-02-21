e5aa0d60ac525f0f3a9e2ca000ad9ee30bd19c17
christina tyagi <102990003+cgtyagi@users.noreply.github.com>
2024-02-21 14:58:00 -0800

    Update makeorder_babyloc_2_short_static_runs.m

--- matlab/combined/makeorder_babyloc_2_short_static_runs.m
+++ matlab/combined/makeorder_babyloc_2_short_static_runs.m
@@ -1,4 +1,4 @@
-function makeorder_babyloc_2_short_static_runs(participant)
+function makeorder_babyloc_2_short_static_runs(participant, user)
 %% Generates static runs (2 short) + CSV scripts for functional
 %% localizer for the infant scans containing 8 stimuli per block with presentation rates of
 %% 2Hz. Last image in a run is a random pattern, to account for lag in measuring frame rate.
@@ -41,7 +41,7 @@ blockdur = stimsperblock * stimdur; % block duration (sec)
 rundur = nblocks * blockdur; % run duration (sec)
 
 % Get user input and concatenate it into the file path
-participant_folder = fullfile('/Users', 'vpnl', 'Desktop', 'bbfloc', 'PsychoPy', 'data', participant, 'combined');
+participant_folder = fullfile('/Users', user, 'Desktop', 'bbfloc', 'PsychoPy', 'data', participant, 'combined');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % GENERATE STIMULUS SEQUENCES
@@ -114,7 +114,7 @@ end
 %%%%%%%%%%%%%%%%%%%
 
 % Path to the directory containing image files
-image_directory = fullfile('/Users', 'vpnl', 'Desktop', 'bbfloc', 'stimuli', 'static_stimuli');
+image_directory = fullfile('/Users', user, 'Desktop', 'bbfloc', 'stimuli', 'static_stimuli');
 
 for r = 1:nruns
     % Adjust the condition for selecting static runs
