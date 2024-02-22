0575b086682ff80ff71527b02a06c4f31c7c4520
christina tyagi <102990003+cgtyagi@users.noreply.github.com>
2024-02-22 13:29:15 -0800

    Update makeorder_babyloc_2_long_grayscale_dyna_runs.m

--- matlab/combined/makeorder_babyloc_2_long_grayscale_dyna_runs.m
+++ matlab/combined/makeorder_babyloc_2_long_grayscale_dyna_runs.m
@@ -1,8 +1,8 @@
 function makeorder_babyloc_2_long_grayscale_dyna_runs(participant, user)
-%% Generates 2 runs/CSV scripts for dynamic GRAYSCALE condition for the infant scans containing 2 stimuli per block with presentation rates of
+%% Generates 2 runs/CSV scripts for dynamic GRAYSCALE condition containing 2 stimuli per block with presentation rates of
 %% long.
 %
-% INPUT: Should be the baby's number 
+% INPUT: Should be the baby's number, the user of the laptop
 % OUTPUTS: Separate script file for each run of psychopy experiment.
 %
 % STIMULI: 5 stimulus conditions (aka categories) 
