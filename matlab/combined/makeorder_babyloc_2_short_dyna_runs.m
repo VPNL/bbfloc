f0f22f6a3b6a2970049d00a7059836a67701589c
christina tyagi <102990003+cgtyagi@users.noreply.github.com>
2024-02-22 13:30:46 -0800

    Update makeorder_babyloc_2_short_dyna_runs.m

--- matlab/combined/makeorder_babyloc_2_short_dyna_runs.m
+++ matlab/combined/makeorder_babyloc_2_short_dyna_runs.m
@@ -1,5 +1,5 @@
 function makeorder_babyloc_2_short_dynaruns(participant, user)
-%% Generates 2 dynamic runs/CSV scripts containing 2 stimuli per block
+%% Generates 2 short dynamic runs/CSV scripts containing 2 stimuli per block
 %
 % INPUT: Should be the baby's number, the user of the laptop
 % OUTPUTS: Separate script file for each run of PsychoPy experiment.
