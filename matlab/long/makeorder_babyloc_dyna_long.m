3086b23c0035024076072c0ef0068cb131978726
christina tyagi <102990003+cgtyagi@users.noreply.github.com>
2024-03-27 15:05:07 -0700

    Update makeorder_babyloc_dyna_long.m

--- matlab/long/makeorder_babyloc_dyna_long.m
+++ matlab/long/makeorder_babyloc_dyna_long.m
@@ -1,6 +1,6 @@
 function makeorder_babyloc_dyna_long(participant, user)
-%% Generates 4 runs/CSV scripts for dynamic condition for the infant scans containing 2 stimuli per block with presentation rates of
-%% long.
+% Generates 4 long-length runs/CSV scripts for dynamic condition for the infant scans containing 1 stimuli per block with presentation rates of
+%% 4s.
 %
 % INPUT: Should be the baby's number 
 % OUTPUTS: Separate script file for each run of PsychoPy experiment.
