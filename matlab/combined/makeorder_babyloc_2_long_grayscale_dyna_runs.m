f90a86fe74788547321835eea8b0d428611b0fe5
christina tyagi <102990003+cgtyagi@users.noreply.github.com>
2024-03-27 14:54:12 -0700

    Update makeorder_babyloc_2_long_grayscale_dyna_runs.m

--- matlab/combined/makeorder_babyloc_2_long_grayscale_dyna_runs.m
+++ matlab/combined/makeorder_babyloc_2_long_grayscale_dyna_runs.m
@@ -1,8 +1,8 @@
-function makeorder_babyloc_2_long_dyna_runs(participant)
+function makeorder_babyloc_2_long_dyna_runs(participant, user)
 %% Generates 2 runs/CSV scripts for dynamic condition for the infant scans containing 2 stimuli per block with presentation rates of
 %% long.
 %
-% INPUT: Should be the baby's number 
+% INPUT: Should be the baby's number, user of the laptop
 % OUTPUTS: Separate script file for each run of psychopy experiment.
 %
 % STIMULI: 5 stimulus conditions (aka categories) 
@@ -40,7 +40,7 @@ ntrials = nblocks*stimsperblock; % number of trials in a run
 blockdur = stimsperblock*stimdur; % block duration (sec)
 rundur = nblocks*blockdur; % run duration (sec)
 
-participant_folder = fullfile('/Users', 'vpnl', 'Desktop', 'bbfloc', 'psychopy', 'data', participant, 'grayscale');
+participant_folder = fullfile('/Users', user, 'Desktop', 'bbfloc', 'psychopy', 'data', participant, 'grayscale');
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -73,8 +73,9 @@ for r = 1:nruns
     condmat(:, r) = reshape(repmat(condvec', stimsperblock, 1), ntrials, 1);
 end
 
-stim_dir = '/Users/vpnl/Desktop/bbfloc/stimuli/dynamic_grayscale_stim';
-blank_video_path = '/Users/vpnl/Desktop/bbfloc/stimuli/dynamic_stimuli/blank/blank.mp4'
+stim_dir = fullfile('/Users', user, 'Desktop', 'bbfloc', 'stimuli', 'dynamic_grayscale_stim');
+blank_video_path = fullfile('/Users', user, 'Desktop', 'bbfloc', 'stimuli', 'dynamic_stimuli', 'blank.mp4')
+
 
 % Create matrix for Image
 vidmat = cell(ntrials,nruns);
@@ -269,7 +270,9 @@ end
 
 
 % Path to the directory containing video files
-video_directory = fullfile('/Users', 'vpnl', 'Desktop', 'bbfloc',  'stimuli', 'dynamic_grayscale_stim');
+video_directory = stim_dir = fullfile('/Users', user, 'Desktop', 'bbfloc', 'stimuli', 'dynamic_grayscale_stim');
+
+
 
 % Map the original category index to a new index
 category_mapping = [9, 10, 11, 12, 0];
