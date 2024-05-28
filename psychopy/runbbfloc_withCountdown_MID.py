adbbf26f6290c7ed883f8ccacdf878f2670a44f3
christina tyagi <102990003+cgtyagi@users.noreply.github.com>
2024-05-28 14:31:12 -0700

    Update runbbfloc_withCountdown_MID.py

--- psychopy/runbbfloc_withCountdown_MID.py
+++ psychopy/runbbfloc_withCountdown_MID.py
@@ -1,3 +1,5 @@
+# --- Loads eight unique long runs: 1) static; 2) static; 3) dynamic; 4) dynamic; 5) static; 6) static; 7) dynamic; 8) dynamic
+
 # --- Import packages ---
 from psychopy import locale_setup
 from psychopy import prefs
