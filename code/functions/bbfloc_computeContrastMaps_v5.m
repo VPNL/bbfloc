aab101f47d5fbc67813d227ad4649b6f658fc59a
christina tyagi <102990003+cgtyagi@users.noreply.github.com>
2026-05-05 11:19:48 -0700

    Update bbfloc_computeContrastMaps_v5.m

--- code/functions/bbfloc_computeContrastMaps_v5.m
+++ code/functions/bbfloc_computeContrastMaps_v5.m
@@ -1,7 +1,7 @@
 function bbfloc_computeContrastMaps_v5(GLM)
 
-% based on compute_allcontrasts from Longitudinal (% VSN 05/2014)
-
+% based on compute_allcontrasts from Longitudinal (% VN 05/2014)
+% CT 4/26
 
 % This code is appropriate localizer scans with
 % parfiles corresponding to the conditions listed below:
@@ -10,11 +10,10 @@ function bbfloc_computeContrastMaps_v5(GLM)
 % 2 limbs-static
 % 3 cars-static
 % 4 scenes-static
-% 5 food-static
-% 6 faces-dynamic
-% 7 limbs-dynamic
-% 8 cars-dynamic
-% 9 scenes-dynamic 
+% 5 faces-dynamic
+% 6 limbs-dynamic
+% 7 cars-dynamic
+% 8 scenes-dynamic 
 %%%%%%%%%%%%
 
 % adapted from Longi for Kids AcrossYears (MN 2019) 
@@ -24,81 +23,89 @@ hI = setCurScan(hI,GLM);
 
 % ALL visual contrasts (sanity checks) 
 hI=computeContrastMap2(hI, [1 2 3 4 5 6 7 8], 0,'Visual_vs_blank','test','T', 'mapUnits', 'T'); 
+hI=computeContrastMap2(hI, [1 2 3 4], 0, 'allStatic_vs_blank','test','T', 'mapUnits', 'T');
+hI=computeContrastMap2(hI, [5 6 7 8], 0, 'allDynamic_vs_blank','test','T', 'mapUnits', 'T'); 
 
 % ALL face contrasts
-hI=computeContrastMap2(hI, 1, [2 3 4], 'Faces-D_vs_allDynamic','test','T', 'mapUnits', 'T'); 
+hI=computeContrastMap2(hI, [1 5], [2 3 4 6 7 8],'faces_vs_all','test','T', 'mapUnits', 'T'); 
+
+hI=computeContrastMap2(hI, 1, [2 3 4], 'Faces-S_vs_allStatic','test','T', 'mapUnits', 'T');
+hI=computeContrastMap2(hI, 1, [2 3 4 6 7 8], 'Faces-S_vs_allNonFaces','test','T', 'mapUnits', 'T');
+
+hI=computeContrastMap2(hI, 5, [6 7 8], 'Faces-D_vs_allDynamic','test','T', 'mapUnits', 'T'); 
+hI=computeContrastMap2(hI, 5, [2 3 4 6 7 8], 'Faces-D_vs_allNonFaces','test','T', 'mapUnits', 'T'); 
 
 % ALL limb contrasts
-hI=computeContrastMap2(hI, 2, [1 3 4], 'Limbs-D_vs_allDynamic','test','T', 'mapUnits', 'T'); 
+hI=computeContrastMap2(hI, [2 6], [1 3 4 5 8 7],'limbs_vs_all','test','T', 'mapUnits', 'T'); 
+
+hI=computeContrastMap2(hI, 2, [1 3 4], 'Limbs-S_vs_allStatic','test','T', 'mapUnits', 'T');
+hI=computeContrastMap2(hI, 2, [1 3 4 5 8 7], 'Limbs-S_vs_allNonLimbs','test','T', 'mapUnits', 'T');
+
+hI=computeContrastMap2(hI, 6, [5 7 8], 'Limbs-D_vs_allDynamic','test','T', 'mapUnits', 'T'); 
+hI=computeContrastMap2(hI, 6, [1 3 4 5 8 7], 'Limbs-D_vs_allNonLimbs','test','T', 'mapUnits', 'T');
 
 % ALL car contrasts
-hI=computeContrastMap2(hI, 3, [1 2 4], 'Cars-D_vs_allDynamic','test','T', 'mapUnits', 'T'); 
+hI=computeContrastMap2(hI, [3 7], [1 2 4 5 6 8],'objects_vs_all','test','T', 'mapUnits', 'T'); 
+
+hI=computeContrastMap2(hI, 3, [1 2 4], 'Objects-S_vs_allStatic','test','T', 'mapUnits', 'T');
+hI=computeContrastMap2(hI, 3, [1 2 4 5 6 8], 'Objects-S_vs_allNonObjects','test','T', 'mapUnits', 'T');
+
+hI=computeContrastMap2(hI, 7, [5 6 8], 'Objects-D_vs_allDynamic','test','T', 'mapUnits', 'T'); 
+hI=computeContrastMap2(hI, 7, [1 2 4 5 6 8], 'Objects-D_vs_allNonObjects','test','T', 'mapUnits', 'T');
 
 % ALL scene contrasts
-hI=computeContrastMap2(hI, 4, [1 2 3], 'Scenes-D_vs_allDynamic','test','T', 'mapUnits', 'T'); 
+hI=computeContrastMap2(hI, [4 8], [1 2 3 5 6 7],'scenes_vs_all','test','T', 'mapUnits', 'T'); 
+
+hI=computeContrastMap2(hI, 4, [1 2 3], 'Scenes-S_vs_allStatic','test','T', 'mapUnits', 'T');
+hI=computeContrastMap2(hI, 4, [1 2 3 5 6 7], 'Scenes-S_vs_allNonScenes','test','T', 'mapUnits', 'T');
+
+hI=computeContrastMap2(hI, 8, [5 6 7], 'Scenes-D_vs_allDynamic','test','T', 'mapUnits', 'T'); 
+hI=computeContrastMap2(hI, 8, [1 2 3 5 6 7], 'Scenes-D_vs_allNonScenes','test','T', 'mapUnits', 'T');
+
 
 % Animate vs inanimate
-hI=computeContrastMap2(hI, [1 2], [3 4],'animate_vs_inanimate','test','T', 'mapUnits', 'T'); 
+hI=computeContrastMap2(hI, [1 2 5 6], [3 4 7 8],'animate_vs_inanimate','test','T', 'mapUnits', 'T'); 
 
 % Faces_vs_scenes
-hI=computeContrastMap2(hI, 1, 4,'faces_vs_scenes','test','T', 'mapUnits', 'T');
+hI=computeContrastMap2(hI, [1 5], [4 8],'faces_vs_scenes','test','T', 'mapUnits', 'T');
 
 
 %% Tally how many times each condition appears - save tally in txt file 
 % complile list of all conditions in parfiles
-%% Tally how many times each condition appears - save tally in txt file 
 load mrInit_params.mat
 load mrSESSION.mat
 
 [cond_nums, conds] = deal([]); cnt = 0;
-for rr = 1:length(params.functionals)  % Adjust if only certain runs are needed
+for rr = 1:length(params.functionals) %% make sure rr is pointing to the parfiles you want (rr = 1, rr = 2)
     fid = fopen(params.parfile{rr}, 'r');
-    if fid == -1
-        error('Cannot open parfile: %s', params.parfile{rr});
-    end
     while ~feof(fid)
-        ln = strtrim(fgetl(fid));
-        if isempty(ln), continue; end  % skip empty lines, but don’t exit
-        ln(ln == sprintf('\t')) = ' ';  % convert tabs to spaces
-        prts = strsplit(ln, ' ');
-        prts(cellfun(@isempty, prts)) = [];
-        if length(prts) >= 3
-            cnt = cnt + 1;
-            cond_nums(cnt) = str2double(prts{2});
-            conds{cnt} = prts{3};
-        end
+        ln = fgetl(fid); cnt = cnt + 1;
+        if isempty(ln); return; end; ln(ln == sprintf('\t')) = ' ';
+        prts = deblank(strsplit(ln, ' ')); prts(cellfun(@isempty, prts)) = [];
+        cond_nums(cnt) = str2double(prts{2});
+        conds{cnt} = prts{3};
     end
     fclose(fid);
 end
-
-% Get list of unique condition numbers and their names
-[cond_num_list, ~, ic] = unique(cond_nums);
-cond_list = cell(size(cond_num_list));
-for i = 1:length(cond_num_list)
-    first_idx = find(cond_nums == cond_num_list(i), 1);
-    cond_list{i} = conds{first_idx};
+% make a list of unique condition numbers and corresponding condition names
+cond_num_list = unique(cond_nums); cond_list = cell(1, length(cond_num_list));
+for cc = 1:length(cond_num_list)
+    cond_list{cc} = conds{find(cond_nums == cond_num_list(cc), 1)};
 end
 
-% Count occurrences
-cond_counts = histcounts(cond_nums, 'BinMethod', 'integers');
+% Count how many times each unique condition number appears
+[cond_num_counts] = histcounts(cond_nums, 'BinMethod', 'integers');
 
-% Write to file
+% Open a text file to save the results
 output_filename = 'condition_counts.txt';
-fid = fopen(output_filename, 'w');
+fid = fopen(output_filename, 'w');  % Open file for writing ('w' mode)
 if fid == -1
-    error('Error opening file for writing: %s', output_filename);
+    error('Error opening file: %s', output_filename);
 end
-
 fprintf(fid, 'Condition | Count\n');
-fprintf(fid, '----------------------------\n');
+fprintf(fid, '---------------------------------\n');
 for i = 1:length(cond_num_list)
-    if i == 1 
-        fprintf(fid, '%s appears %d times\n', cond_list{1}, cond_counts(1));
-    else
-        fprintf(fid, '%s appears %d times\n', cond_list{i}, cond_counts(i+4));
-    end
+    fprintf(fid, '%s appears %d times\n', cond_list{i}, cond_num_counts(i));
 end
 fclose(fid);
-
 fprintf('Tallying has been saved to %s\n', output_filename);
-
