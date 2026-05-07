f84fb10835a4bbb5d3fb27b29c2040f878e7884c
christina tyagi <102990003+cgtyagi@users.noreply.github.com>
2026-05-07 16:18:44 -0700

    Update bbfloc_R1_per_ROI_group.m

--- across age analyses/babies/stats (avgd. across rois)/bbfloc_R1_per_ROI_group.m
+++ across age analyses/babies/stats (avgd. across rois)/bbfloc_R1_per_ROI_group.m
@@ -52,20 +52,15 @@ for s = 1:length(session)
 
    for c = 1:length(contrast)
         
-
-        % Identify relevant ROIs depending on contrast type
-        if strcmp(contrast(c).name, 'faces')
-            roi_idx = find(contains(session(currSession).ROIs,'faces') | ...
-                           contains(session(currSession).ROIs,'Faces'));
-        elseif strcmp(contrast(c).name, 'places')
-            roi_idx = find(contains(session(currSession).ROIs,'CoS') | ...
-                           contains(session(currSession).ROIs,'PPA'));  % FIX 2: removed duplicate CoS condition
-        elseif strcmp(contrast(c).name, 'visual')
-            roi_idx = find(contains(session(currSession).ROIs,'v1') | ...
-                           contains(session(currSession).ROIs,'V1'));
-        else
-            roi_idx = find(contains(session(currSession).ROIs,'limbs') | ...
-                           contains(session(currSession).ROIs,'limbs'));
+        %Identify relevant ROIs depending on contrast type
+        if strcmp(contrast(c).name,'faces') % if contrast = faces vs all  - continue analysis on fusiform ROIs
+            roi_idx=find(contains(session(currSession).ROIs,'Fus') | contains(session(currSession).ROIs,'fus'));
+        elseif strcmp(contrast(c).name,'limbs') 
+           roi_idx=find(contains(session(currSession).ROIs,'OTS') | contains(session(currSession).ROIs,'ots') );         
+        elseif strcmp(contrast(c).name,'places') % if contrast = places vs all - continue analysis on CoS ROIs
+            roi_idx=find(contains(session(currSession).ROIs,'CoS')| contains(session(currSession).ROIs,'CoS')| contains(session(currSession).ROIs,'PPA'));
+        else % if contrast = visual vs blank - continue analysis on v1 ROIs
+           roi_idx=find(contains(session(currSession).ROIs,'v1') | contains(session(currSession).ROIs,'V1') ); 
         end
 
         contrastName = string(contrast(c).name);
